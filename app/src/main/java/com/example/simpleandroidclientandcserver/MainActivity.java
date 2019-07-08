c9dd7747aa52435115b80c104ffe403599e538fa
Ruiyang Zhu <ryanzhu@umich.edu>
2019-07-08 15:12:59 -0400

    initial commit

---
+++ app/src/main/java/com/example/simpleandroidclientandcserver/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.simpleandroidclientandcserver;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    String test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread connect = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Socket sock = new Socket("192.168.1.131", 1234);
+                            BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                            test = br.readLine();
+                            Log.d("msg:", test);
+                            sock.close();
+
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                            Log.d("SocketFail", "Socket exception");
+                        }
+                    }
+                });
+                connect.start();
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
